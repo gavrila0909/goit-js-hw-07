5372732d0ac4d55307b50882c2521fcc058bcba1
gavrila0909 <gavriladanna@gmail.com>
2024-01-20 11:52:02 +0000

    Done tasks 1 and 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const createGalleryItems = galleryItems
+  .map(({ original, preview, description }) => 
+    `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-src="${original}" alt="${description}" />
+      </a>
+    </li>`
+  ).join("");
+
+galleryContainer.insertAdjacentHTML('beforeend', createGalleryItems);
+
+galleryContainer.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (event.target.nodeName === 'IMG') {
+    const imageUrl = event.target.dataset.src;
+    const lightboxInstance = basicLightbox.create(`
+      <img src="${imageUrl}" alt="${event.target.alt}" />
+    `);
+
+    lightboxInstance.show();
+  }
+});
